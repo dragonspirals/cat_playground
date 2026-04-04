6b9caf9a822e6b0f75981c64c1bd119377f29ef9
dragonspirals <leeradja99@gmail.com>
2026-04-04 19:36:37 +0100

    remove comments

--- src/app/components/Rod.ts
+++ src/app/components/Rod.ts
@@ -15,7 +15,7 @@ export class Rod extends BackpackItem
     constructor(settings: DynamicObjectSettings)
     {
         super(settings)
-        for (let i=0; i< 11; i++)
+        for (let i=0; i< 50; i++)
         {
 
             const rodSection = new RodSection();
@@ -29,27 +29,16 @@ export class Rod extends BackpackItem
         super.update(container)
         this._rodSections.forEach((section) => section.update())
         this.drawUpdate();
-        
-        // if (this._rotVel === 0 && this._theta === 0) { return; }
-        // const rotAccel = -0.1 * Math.sin(this._theta) 
-        // this.theta = this._theta + rotAccel
     }
 
     protected handleMouseMove(e: PIXI.FederatedMouseEvent)
     {
         if (!this._isDragging.value || !this.parent) { return; }
-        // this.position = this.parent.toLocal(e.global)
-        // const endDirection = {x:this._endPos.x - e.movementX, y: this._endPos.y - e.movementY}
-        // const distance = Magnitude(endDirection)
-        // this._endPos = { x: endDirection.x * this._radius/distance, y: endDirection.y * this._radius/distance}
-        // this._theta = Math.atan(this._endPos.x/Math.abs(this._endPos.y))
         this._rodSections[0].updateStartPos(this.toLocal(e.global))
         for (let i=1; i<this._rodSections.length; i++)
         {
             this._rodSections[i].updateStartPos(this._rodSections[i-1].endPos)
         }
-        // this._section2.updateStartPos(this._rodSection.endPos)
-        // this.drawUpdate();    
     }
 
     protected drawUpdate()
@@ -58,7 +47,6 @@ export class Rod extends BackpackItem
         this.graphics.clear();
         this.graphics.moveTo(this._rodSections[0].startPos.x, this._rodSections[0].startPos.y)
             .lineTo(this._rodSections[0].endPos.x, this._rodSections[0].endPos.y)
-            // .stroke({ color: "#6b1000", width: 20 })
 
         for (let i=1; i<this._rodSections.length; i++)
         {
@@ -71,7 +59,6 @@ export class Rod extends BackpackItem
     {       
         const object = new PIXI.Graphics();   
         this.addChild(object)
-        this._object = object                
-        // this.drawUpdate()           
+        this._object = object       
     }
 }
--- src/app/components/RodSection.ts
+++ src/app/components/RodSection.ts
@@ -7,26 +7,11 @@ export class RodSection
     public get startPos() : VECTOR.Position { return this._startPos}
     private _endPos: VECTOR.Position = { x: 0, y: this._radius }
     public get endPos() { return this._endPos }
-    private _rotVel: number = 0;
-
-    /**theta measured from vertical downwards */
-    // private _theta: number = 0;
-    // public set theta(val: number)
-    // {
-    //     this._theta = val % (Math.PI * 2)
-    //     this._endPos = { x: this._radius * Math.sin(this._theta) - this._startPos.x, y: this._radius * Math.cos(this._theta) - this.startPos.y}
-    // }
+    
     constructor() { }
 
     public update()
     {
-        // if (this._rotVel === 0 && this._theta === 0) { return; }
-        // const direction = VECTOR.Subtract(this._endPos, this.startPos)
-        // const theta = Math.atan(direction.x/Math.abs(direction.y))
-        // const rotAccel = -0.1 * Math.sin(theta) 
-        // const newTheta = 0.98 * (theta + rotAccel)
-        // this._endPos = {x: this._radius * Math.sin(newTheta) + this._startPos.x, y:  this._radius * Math.cos(newTheta) + this._startPos.y}
-
     }
 
     public updateStartPos(startPos: VECTOR.Position)
@@ -34,7 +19,6 @@ export class RodSection
         const direction = VECTOR.Subtract(this._endPos, startPos)
         const distance = VECTOR.Magnitude(direction)
         const newEndPos = { x: direction.x * this._radius/distance + startPos.x, y: direction.y * this._radius/distance + startPos.y}
-        // this._theta = Math.atan(this._endPos.x/Math.abs(this._endPos.y))
         this._startPos = startPos
         const movement = VECTOR.Subtract(this._endPos, newEndPos)
         if (movement.x !== 0 || movement.y !== 0) { console.log(movement) }
